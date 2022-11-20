2631b60f4f3227d48c17d08dc43246b21629c4c2
milonroy26 <101338962+milonroy26@users.noreply.github.com>
2022-11-21 00:32:57 +0600

    class sidebar create and website done

---
+++ main.js
@@ -0,0 +1,25 @@
+const sidebar = document.querySelector('.sidebar');
+const navItems = document.querySelectorAll('nav .nav-item');
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', () => {
+    if (sidebar.className === 'sidebar') {
+        sidebar.classList.add('open');
+    }
+
+    else {
+        sidebar.classList.remove('open');
+    }
+})
+
+navItems.forEach(navItem => {
+
+    navItem.addEventListener('click', () => {
+
+        navItems.forEach(navItem => {
+            navItem.classList.remove('active');
+        })
+
+        navItem.classList.add('active');
+    })
+})
